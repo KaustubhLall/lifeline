4344c7cfc3ec4290a7c4dfac8f837cde26a20a9c
Kaus <kaustubhlall@gmail.com>
2025-06-28 19:23:43 -0700

    refactor api backend

---
+++ backend/LifeLine/api/views/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# login and auth views
+
+# start new conversation
+# get messages by conversation
+# get new response for a give conversation by user input
+# view all user models and notes on frontend
